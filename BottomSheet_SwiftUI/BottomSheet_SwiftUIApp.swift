50e0d758e84f3389b0ad8d6d4040536580d5a9af
KimWooSung <kipsong133@gmail.com>
2022-02-06 14:34:15 +0900

    Initial Commit

---
+++ BottomSheet_SwiftUI/BottomSheet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomSheet_SwiftUIApp.swift
+//  BottomSheet_SwiftUI
+//
+//  Created by 김우성 on 2022/02/06.
+//
+
+import SwiftUI
+
+@main
+struct BottomSheet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BottomSheet_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BottomSheet_SwiftUI
+//
+//  Created by 김우성 on 2022/02/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
